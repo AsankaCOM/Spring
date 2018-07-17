72104373c94cc517db10d0ab1b4a585461ab4add
AsankaCOM <9785008+AsankaCOM@users.noreply.github.com>
2018-07-17 09:45:56 -0600

    Update ReservationResository.java

--- CarRegistrationMVC/src/main/java/com/ecaphi/cars/repo/ReservationResository.java
+++ CarRegistrationMVC/src/main/java/com/ecaphi/cars/repo/ReservationResository.java
@@ -7,7 +7,5 @@ import org.springframework.stereotype.Repository;
 import com.ecaphi.cars.model.Car;
 
 @Repository
-//public interface ReservationResository extends JpaRepository<Car, Long> {
 public interface ReservationResository extends CrudRepository<Car, Integer> {
-
 }
