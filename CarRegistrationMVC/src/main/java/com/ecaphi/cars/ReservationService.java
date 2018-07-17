cfc94e8cc086a4ab0a7bd9c4a94bb29457060333
AsankaCOM <9785008+AsankaCOM@users.noreply.github.com>
2018-07-17 09:48:43 -0600

    Update ReservationService.java

--- CarRegistrationMVC/src/main/java/com/ecaphi/cars/ReservationService.java
+++ CarRegistrationMVC/src/main/java/com/ecaphi/cars/ReservationService.java
@@ -4,15 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-//import com.ecaphi.cars.dao.CarDao;
 import com.ecaphi.cars.model.Car;
 import com.ecaphi.cars.repo.ReservationResository;
 
 @Service
 public class ReservationService {
 
-	//private CarDao dao;
 	private ReservationResository reservationResository;
 	
 	@Autowired
@@ -24,40 +21,29 @@ public class ReservationService {
 	
 	public List<Car> getAllCars() 
 	{
-		//return this.dao.getAllCars();
-		
 		List<Car> cars = new ArrayList<>();
 		this.reservationResository.findAll().forEach(cars::add);
 		
 		return cars;
 	}
 	
-	
 	public void addCar(Car car)
 	{
-		//this.dao.addCar(car);
 		this.reservationResository.save(car);
-	}
-	
+	}	
 
 	public void deleteCar(int carId)
 	{
-		//this.dao.deleteCar(carId);
 		this.reservationResository.deleteById(carId);
 	}
 	
 	public void updateCar(Car car)
 	{
-		//this.dao.updateCar(car);
 		this.reservationResository.save(car);
 	}
 	
 	public Car getCarById(int carId)
 	{
-		//return this.dao.getCarById(carId);
-		//return this.reservationResository.findById(carId).get();
 		return this.reservationResository.findById(carId).get();
-		
-	}	
-	
+	}		
 }
